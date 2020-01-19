4f0c115b14ee913050a352ccbdfbd33070956b79
Mahmoud Kassah <kassah93@gmail.com>
2020-01-19 21:07:02 +0200

    deploy

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,2 +1,2 @@
-export  const baseUrl = process.env.BASE_URL || "http://localhost/api/";
-//export  const baseUrl = "http://aboaljoud94-001-site1.atempurl.com/api/";
+//export  const baseUrl = process.env.BASE_URL || "http://localhost/api/";
+export  const baseUrl = "http://aboaljoud94-001-site1.atempurl.com/api/";
